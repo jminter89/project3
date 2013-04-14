8a85df5c9fb2d4f23c550ad6afaf51cd5bfb0a08
Joseph Minter <jminter89@gmail.com>
2013-04-14 14:54:00 -0700

    final Proj3-1

--- sgemm-small.c
+++ sgemm-small.c
@@ -1,7 +1,8 @@
 #include <nmmintrin.h>
 #include <string.h>
+#include <stdio.h>
 
-#define STRIDE 4
+#define STRIDE 8
 
 void sgemm( int m, int n, int d, float *A, float *C )
 {
@@ -66,35 +67,60 @@ void sgemm( int m, int n, int d, float *A, float *C )
     }
 
   } else {
-    __m128 a2_1, a1, c;
+    __m128 a2_1, a2_2, a1, c_1, c_2, c_3, c_4;
     register int k,j,i;
     
-    int addRow = n - (n%STRIDE);
-    int addCol = d+n - ((d+n)%STRIDE);
-    int padRow = addRow + STRIDE;
-    int padCol = addCol + STRIDE;
-    int padSize = padRow * padCol;
-    float Apad[padSize];
+    const int addRow = n - (n%STRIDE);
+   	const int padRow = addRow + STRIDE;
+    const int padSizeA = padRow * (d+n);
+    const int padSizeC = padRow * n;
+    float Apad[padSizeA];
+    float Cpad[padSizeC];
     
-    for(j = 0; j < n; j++){
-      memcpy((float*)(Apad + padRow*j), A + n*j, n*sizeof(float));
-      memset((float*)(Apad + padRow*j + n), 0, (padRow - n)*sizeof(float));
-    }
-    memset((float*)(Apad + padRow*j), 0, padRow*(padCol - (d+n))*sizeof(float));
-    
-    for( k = 0; k < m; k++ ){ 
-      for( j = 0; j < n; j++ ){
-	 a2_1 = _mm_load1_ps(Apad+j*(n+1)+k*(n));
-	 for( i = 0; i < n; i+=STRIDE ){
-	  
-	  a1 = _mm_loadu_ps(Apad+i+k*(n)); 
-	  
-	  c = _mm_loadu_ps(C+i+j*(n));
-	  c = _mm_add_ps(c, _mm_mul_ps(a1, a2_1));
-	  _mm_storeu_ps(C+i+j*(n), c);
-	 }
-      }
+    for(j = 0; j < (d+n); j++){
+      	memcpy((float*)(Apad + padRow*j), A + n*j, n*sizeof(float));
+      	memset((float*)(Apad + padRow*j+n), 0, (padRow-n)*sizeof(float));
     }
 
+   
+    for(j = 0; j < n; j++){	
+    	memcpy((float*)(Cpad + padRow*j), C + n*j, n*sizeof(float));
+    	memset((float*)(Cpad + padRow*j + n), 0, (padRow - n)*sizeof(float));
+    }
+    
+    for( k = 0; k < m; k++ ){ 
+    	for( j = 0; j < n; j+=2 ){
+	  		a2_1 = _mm_load1_ps(Apad+j*(padRow+1)+k*(padRow));
+	  		a2_2 = _mm_load1_ps(Apad+(j+1)*(padRow+1)+k*(padRow));
+		 		for( i = 0; i < n; i+=STRIDE ){
+	  
+	  			a1 = _mm_loadu_ps(Apad+i+k*(padRow)); 
+	  
+	  			c_1 = _mm_loadu_ps(Cpad+i+j*(padRow));
+	  			c_1 = _mm_add_ps(c_1, _mm_mul_ps(a1, a2_1));
+
+	  			c_2 = _mm_loadu_ps(Cpad+i+(j+1)*(padRow));
+	  			c_2 = _mm_add_ps(c_2, _mm_mul_ps(a1, a2_2));
+
+	  			_mm_storeu_ps(Cpad+i+j*(padRow), c_1);
+	  			_mm_storeu_ps(Cpad+i+(j+1)*(padRow), c_2);
+
+	  			a1 = _mm_loadu_ps(Apad+i+4+k*(padRow)); 
+	  
+	  			c_3= _mm_loadu_ps(Cpad+i+4+j*(padRow));
+	  			c_3 = _mm_add_ps(c_3, _mm_mul_ps(a1, a2_1));
+
+	  			c_4= _mm_loadu_ps(Cpad+i+4+(j+1)*(padRow));
+	  			c_4 = _mm_add_ps(c_4, _mm_mul_ps(a1, a2_2));
+
+	  			_mm_storeu_ps(Cpad+i+4+j*(padRow), c_3);
+	  			_mm_storeu_ps(Cpad+i+4+(j+1)*(padRow), c_4);
+	 		}
+      	}
+    }
+
+    for(j = 0; j < n; j++){	
+    	memcpy((float*)(C + n*j), Cpad + j*padRow, n*sizeof(float));
+    }
   }
 }
